081c9e2a50700c109c6bd5712fc9bc842dd40e16
Pedro Lucas <pedro.lucas0801@outlook.com>
2020-04-01 10:38:49 -0300

    Resolução do exercício 7

---
+++ exercicio_07d.py
@@ -0,0 +1,32 @@
+'''
+Resolução do professor: Paulo Andre Vechiatto de Miranda
+
+Problema:
+Sabe-se que cada número da forma n**3 é igual a soma de n ímpares consecutivos.
+
+Exemplos:
+1**3 = 1,
+2**3 = 3 + 5,
+3**3 = 7 + 9 + 11,
+4**3 = 13 + 15 + 17 + 19.
+
+Dado um número inteiro m, m > 0, determinar os ímpares consecutivos cuja soma é igual a n**3, para n assumindo valores de 1 a m.
+'''
+
+m = int(input('Digite m: '))
+n = 1
+inicio = 1
+
+while n <= m:      
+  print('%d**3'%n, '=', inicio, end=' ')
+  i = 2
+  impar = inicio + 2
+
+  while i <= n:
+    print('+', impar, end=' ')
+    impar += 2
+    i += 1
+
+  print()
+  inicio = impar
+  n += 1
